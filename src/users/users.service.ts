b94fa1babf2a27ec128fcdb302a11ecc179cfc4f
Sol0205 <solmargarit3@gmail.com>
2024-08-31 16:31:28 -0300

    - Creo la base de datos. - Intento unir Docker y Postgres a mi project pero hubieron errores por las versiones que tengo. - Esta para corregir.

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,38 +1,45 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
+import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
 export class UsersService {
 
-    private users: any[] = [
-        {
-            id: "1",
-            name: "Sol",
-            phone: "1125723013",
-        },
-        {
-            id: "2",
-            name: "Alex",
-            phone: "1125953726",
-        },
-        {
-            id: "3",
-            name: "Angel",
-            phone: "0123456789",
-        },
-    ];
+
+    constructor(private prisma: PrismaService) { }
+
 
     getUsers() {
         // return ["H", "E", "L", "L", "O"]
-        return this.users;
+        return this.user.findMany();
     }
 
     createUser(user: CreateUserDto) {
-        this.users.push(user)
-
         return {
-            ...user,
-            id: this.users.length + 1,
+            return this.prisma.user.create({
+                data:
+            })
         }
     }
 }
+
+
+//ESTO ESTABA EN LA LINEA 7
+
+// private users: any[] = [
+//     {
+//         id: "1",
+//         name: "Sol",
+//         phone: "1125723013",
+//     },
+//     {
+//         id: "2",
+//         name: "Alex",
+//         phone: "1125953726",
+//     },
+//     {
+//         id: "3",
+//         name: "Angel",
+//         phone: "0123456789",
+//     },
+// ];
