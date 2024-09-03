b01cd49c492f4cb7f518568a2966d2d18aed0673
Sol0205 <solmargarit3@gmail.com>
2024-09-03 10:24:53 -0300

    - Cambio el tipo de identificado de CUID a UUID - Agrego el esquema de la base de datos de Products - Modifico los valores del esquema de la base de datos de Users - Elimino comentarios y codigo redundante

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,100 +1,57 @@
-import { isNotEmpty, IsString, MinLength } from 'class-validator'
+import { IsArray, IsBoolean, IsDate, IsISO8601, isISO8601, IsNumber, IsString, MinLength } from 'class-validator'
 
 export class CreateProductDto {
     @IsString()
     @MinLength(1)
-    name: string
+    nameProduct: string
 
     @IsString()
     @MinLength(1)
     description: string
 
-    //modificarlo y hacer un array de strings
-    //hacerlo en excalidraw
-    //porque  vamos a soportar multiples categorias
-
-    // @IsString()
-    // @MinLength(1)
-    // category: string []
+    @IsArray()
+    @IsString({ each: true })
+    @MinLength(1, { each: true })
+    category: string[]
 
     @IsString()
     @MinLength(1)
-    dimensions: string
+    dimension: string
 
     @IsString()
     @MinLength(1)
     color: string
 
-    // @IsString()
-    // @MinLength(1)
-    // Foto: string
-
-    @IsString()
-    @MinLength(1)
+    @IsNumber()
     price: number
 
     @IsString()
     @MinLength(1)
-    carency: string
+    currency: string
 
-    @IsString()
-    @MinLength(1)
+    @IsNumber()
     quantity: number
 
-    @IsString()
-    @MinLength(1)
+    @IsBoolean()
     available: boolean
 
-    @IsString()
-    @MinLength(1)
+    @IsBoolean()
     featured: boolean
 
-    @IsString()
-    @MinLength(1)
-    creationDate: Date
+    @IsISO8601()
+    creationDate: string
 
-    @IsString()
-    @MinLength(1)
-    updateDate: Date
+    @IsISO8601()
+    updateDate: string
 
     @IsString()
     @MinLength(1)
     creationUser: string
 
-    // @IsString()
-    // @MinLength(1)
-    // usuarioActualizacion: string
-
-    @IsString()
-    @MinLength(1)
-    asset: boolean
-
-    @IsString()
-    @MinLength(1)
+    @IsBoolean()
     deleted: boolean
 
-    // @IsString()
-    // @MinLength(1)
-    // fechaEliminacion: Date
+    @IsString()
+    @MinLength(1)
+    deletionDate: string
 }
-
-// 1. Cambiar el tipo de dato de category a string[]
-// 2. Cambiar el tipo de dato de price a number
-// 3. Cambiar el tipo de dato de quantity a number
-// 4. Cambiar el tipo de dato de available a boolean
-// 5. Cambiar el tipo de dato de featured a boolean
-// 6. Cambiar el tipo de dato de creationDate a Date
-// 7. Cambiar el tipo de dato de updateDate a Date
-// 8. Cambiar el tipo de dato de creationUser a string
-// 9. Cambiar el tipo de dato de asset a boolean
-// 10. Cambiar el tipo de dato de deleted a boolean
-// 11. Cambiar el tipo de dato de fechaEliminacion a Date
-// 12. Cambiar el nombre de Foto a photo
-// 13. Cambiar el nombre de usuarioActualizacion a updateUserName
-// 14. Cambiar el nombre de fechaEliminacion a deletionDate
-// 15. Cambiar el nombre de carency a currency
-// 16. Cambiar el nombre de dimensions a dimension
-// 17. Cambiar el nombre de color a color
-
-
-
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, HttpCode, NotFoundException } from "@nestjs/common";
+import { Injectable, NotFoundException } from "@nestjs/common";
 import { CreateProductDto } from "./dto/create-product.dto";
 import { UpdateProductDto } from "./dto/update-product.dto";
 
